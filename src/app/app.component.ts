3636dfecc8b23dd09d9727b1251c2dc182e64972
shivam <shivammistry2003@gmail.com>
2024-10-01 01:14:30 +0530

    interpolation & property binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,22 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    // selector: 'app-root', // as a tag
+    selector: '.app-root', // as a class
+    // selector: '[app-root]', // as a attribure
+    standalone: true,
+    imports: [RouterOutlet],
+    templateUrl: './app.component.html',
+    // template: `<h1 class="heading">Hello From Inline Template</h1>`,
+    styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'hello-world';
+    public title: string = 'hello-world';
+    public userName: string = 'Shivam';
+    public wantVisible: boolean = true;
+    public imageUrl: string =
+        'https://www.aceinfoway.com/blog/wp-content/uploads/2023/03/javascript-vs-typescript_-what-differs-javascript-and-typescript_.jpg';
+    public greetings(): string {
+        return 'Hello sir, Welcome ' + this.userName;
+    }
 }
