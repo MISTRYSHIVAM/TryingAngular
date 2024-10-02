dcd2c5a2a074e709298075e93f07e048f2a4b6bc
shivam <shivammistry2003@gmail.com>
2024-10-02 19:24:42 +0530

    class,style,event,two-way binding & structural directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
+import { CommonModule, NgClass, NgStyle } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -6,7 +8,7 @@ import { RouterOutlet } from '@angular/router';
     selector: '.app-root', // as a class
     // selector: '[app-root]', // as a attribure
     standalone: true,
-    imports: [RouterOutlet],
+    imports: [RouterOutlet, NgClass, NgStyle, FormsModule, CommonModule],
     templateUrl: './app.component.html',
     // template: `<h1 class="heading">Hello From Inline Template</h1>`,
     styleUrl: './app.component.css',
@@ -20,4 +22,59 @@ export class AppComponent {
     public greetings(): string {
         return 'Hello sir, Welcome ' + this.userName;
     }
+
+    public divClass = 'cb';
+    public hasError = true;
+    public classList = {
+        headingError: !this.hasError,
+        headingSuccess: true,
+        HeadingFull: true,
+    };
+
+    public myStyle = {
+        color: 'blue',
+        fontStyle: 'italic',
+    };
+    public isDanger = true;
+
+    public fieldUpdate() {
+        console.log('hiiii');
+    }
+    public fieldUpdate2(event: KeyboardEvent) {
+        // console.log(event);
+        alert('You press : ' + event.code);
+    }
+    public fieldUpdate3() {
+        alert('you press #');
+    }
+    public box = {
+        backgroundColor: 'skyblue',
+        height: '200px',
+        width: '200px',
+        marginBottom: '10px',
+    };
+    public wranning() {
+        alert('to aap aa hi gaye is area me');
+    }
+    public colorArr = ['red', 'yellow', 'green', 'gray', 'pink'];
+    public colorCounter = 0;
+    public tragedy() {
+        if (this.colorCounter == 4) {
+            this.colorCounter = 0;
+        }
+        this.colorCounter++;
+    }
+
+    public getInputValue(myInputString: string, myButton: HTMLElement) {
+        console.log(myButton);
+        alert(myInputString);
+    }
+
+    public inputText: string = '';
+
+    public displayElement = true;
+    public choice = 'study';
+    public colorForHeading = ['red', 'orange', 'darkgreen', 'blue'];
+
+    public border = '1px solid black';
 }
